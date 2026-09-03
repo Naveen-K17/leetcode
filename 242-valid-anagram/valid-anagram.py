2c74e6e76c509f31b24ca93b0a23358fc5797d17
Naveen Siddappa Kattimani <naveenkattimani326@gmail.com>
2026-09-03 23:26:19 +0530

    Time: 11 ms (77.30%) | Memory: 19.4 MB (46.11%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.py
@@ -0,0 +1,25 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+
+        count = {}
+
+        for char in s:
+            count[char] = count.get(char, 0) + 1
+
+        for char in t:
+            if char not in count:
+                return False
+
+            count[char] -= 1
+
+        for value in count.values():
+            if value != 0:
+                return False
+
+        return True
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
